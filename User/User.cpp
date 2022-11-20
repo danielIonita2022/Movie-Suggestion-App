040c38d448ea9cc580afbfa19d7cac6c0b4f30ce
ana-mariagg <ana.grigoras@student.unitbv.ro>
2022-11-20 19:24:01 +0200

    User.cpp
    The implementation for the constructor and getters.

--- User/User.cpp
+++ User/User.cpp
@@ -1,10 +1,28 @@
 #include "User.h"
 
-User::User(std::string firstName, std::string lastName, std::string userName,std::string password) :
+User::User(std::string firstName, std::string lastName, std::string userName, std::string password, std::string favMovie, std::string favActor,
+	bool drama, bool action, bool SF, bool comedy, bool thriller, bool fantasy, bool animation, bool horror, bool romance, bool mistery, bool adventure,
+	uint16_t year1, uint16_t year2, uint16_t movieSeries) :
 	m_firstName(firstName),
-	m_lastName(lastName), 
+	m_lastName(lastName),
 	m_userName(userName),
-	m_password(password)
+	m_password(password),
+	m_favMovie(favMovie),
+	m_favActor(favActor),
+	m_drama(drama),
+	m_action(action),
+	m_SF(SF),
+	m_comedy(comedy),
+	m_thriller(thriller),
+	m_fantasy(fantasy),
+	m_animation(animation),
+	m_horror(horror),
+	m_romance(romance),
+	m_mistery(mistery),
+	m_adventure(adventure),
+	m_year1(year1),
+	m_year2(year2),
+	m_movieSeries(movieSeries)
 {
 }
 
@@ -47,3 +65,86 @@ void User::SetPassword(const std::string& password)
 {
 	m_password = password;
 }
+
+std::string User::GetFavMovie() const
+{
+	return m_favMovie;
+}
+
+std::string User::GetFavActor() const
+{
+	return m_favActor;
+}
+
+bool User::GetDrama() const
+{
+	return m_drama;
+}
+
+bool User::GetAction() const
+{
+	return m_action;
+}
+
+bool User::GetSF() const
+{
+	return m_SF;
+}
+
+bool User::GetComedy() const
+{
+	return m_comedy;
+}
+
+bool User::GetThriller() const
+{
+	return m_thriller;
+}
+
+bool User::GetFantasy() const
+{
+	return m_fantasy;
+}
+
+bool User::GetAnimation() const
+{
+	return m_animation;
+}
+
+bool User::GetHorror() const
+{
+	return m_horror;
+}
+
+bool User::GetRomance() const
+{
+	return m_romance;
+}
+
+bool User::GetMistery() const
+{
+	return m_mistery;
+}
+
+bool User::GetAdventure() const
+{
+	return m_adventure;
+}
+
+uint16_t User::GetYear1() const
+{
+	return m_year1;
+}
+
+uint16_t User::GetYear2() const
+{
+	return m_year2;
+}
+
+uint16_t User::GetMovieSeries() const
+{
+	return m_movieSeries;
+}
+
+
+
