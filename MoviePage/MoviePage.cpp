6e9d874f178657b45ebc7282a06787786e31ef4d
danielIonita2022 <daniel.ionita@student.unitbv.ro>
2022-11-30 12:08:45 +0200

    Modified MoviePage.cpp
    Implemented the updated search function in MoviePage using regex, and the search will also include movies that have some of the words of the original search (i.e. searching for Jaws 2 will also return Jaws)
    NOTE: This functionality can be improved upon in later iterations

--- MoviePage/MoviePage.cpp
+++ MoviePage/MoviePage.cpp
@@ -1,30 +1,73 @@
 #include "MoviePage.h"
-
-MoviePage::MoviePage(App* app)
+#include <regex>
+MoviePage::MoviePage()
 {
-	this->app = app;
 }
-void MoviePage::ShowDetails(const std::string& title)
+void MoviePage::ShowDetails()
 {
-	movie = getMovie(title);
-	std::cout << movie[0].m_type << '\n';
-	std::cout << movie[0].m_title << '\n';
-	std::cout << movie[0].m_director << '\n';
-	std::cout << movie[0].m_cast << '\n';
-	std::cout << movie[0].m_country << '\n';
-	std::cout << movie[0].m_dateAdded << '\n';
-	std::cout << movie[0].m_releaseYear << '\n';
-	std::cout << movie[0].m_rating << '\n';
-	std::cout << movie[0].m_duration << '\n';
-	std::cout << movie[0].m_listedIn << '\n';
-	std::cout << movie[0].m_description << '\n';
-
+	std::cout << "Cautati un film sau serial dupa titlu: ";
+	std::string title;
+	std::getline(std::cin, title);
+	m_movies = getMovies(title);
+	for (const auto& movie : m_movies)
+	{
+		std::cout << '\n' << movie.m_type << '\n';
+		std::cout << movie.m_title << '\n';
+		std::cout << movie.m_director << '\n';
+		std::cout << movie.m_cast << '\n';
+		std::cout << movie.m_country << '\n';
+		std::cout << movie.m_dateAdded << '\n';
+		std::cout << movie.m_releaseYear << '\n';
+		std::cout << movie.m_rating << '\n';
+		std::cout << movie.m_duration << '\n';
+		std::cout << movie.m_listedIn << '\n';
+		std::cout << movie.m_description << '\n';
+	}
 }
-
-std::vector<Movie> MoviePage::getMovie(const std::string& name)
+int countWordsRegex(const std::string& name)
 {
-	auto table = app->getStorage();
-	auto movie = table.get_all<Movie>(
-		sqlite_orm::where(sqlite_orm::is_equal(&Movie::m_title, name)));
-	return movie;
+	std::regex regex("(\\w+)|(:)");
+	auto wordsBegin = std::sregex_iterator(name.begin(), name.end(), regex);
+	auto wordsEnd = std::sregex_iterator();
+	return std::distance(wordsBegin, wordsEnd);
+}
+std::string::iterator findPosOfLastWord(std::string& name)
+{
+	for (std::string::iterator pos = name.end() - 1; pos != name.begin(); --pos)
+	{
+		if (!isalnum(*pos))
+			return pos;
+	}
+}
+void deleteLastWord(std::string& name)
+{
+	name.erase(findPosOfLastWord(name), name.end());
+}
+std::vector<Movie> MoviePage::getMovies(const std::string& name)
+{
+	auto table = Storages::getInstance()->getMovieStorage();
+	std::vector<Movie> allMovies;
+	std::string incompleteName = name;
+
+	allMovies = table.get_all<Movie>(sqlite_orm::where
+	(sqlite_orm::like((&Movie::m_title), name)));
+
+	int nrWords = countWordsRegex(name);
+	if (nrWords > 1)
+	{
+		for (int count = 1; count < nrWords; ++count)
+		{
+			deleteLastWord(incompleteName);
+			if (incompleteName.empty()) break;
+			std::vector <Movie> movie = table.get_all<Movie>(sqlite_orm::where
+			(sqlite_orm::like((&Movie::m_title), incompleteName)));
+			if (!movie.empty())
+			{
+				allMovies.push_back(movie[0]);
+			}
+		}
+	}
+	if (allMovies.empty())
+		std::cout << "The search query returned no results!\n";
+	return allMovies;
 }
