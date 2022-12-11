ed377a7921bb9a714c73a6cf7caa02bb2583180e
ana-mariagg <ana.grigoras@student.unitbv.ro>
2022-12-11 21:26:34 +0200

    Update RecommendedPage.h
    I made the recommendations for an user based on his wishlist and seen lists. Next week will be improved.

--- RecommendedPage/RecommendedPage.h
+++ RecommendedPage/RecommendedPage.h
@@ -2,15 +2,80 @@
 #include "Storages.h"
 #include "Movie.h"
 #include "User.h"
+#include "Seen.h"
 #include <vector>
+#include <unordered_set>
+
+
 class RecommendedPage
 {
 public:
-	RecommendedPage(const User& currentUser);
-	std::vector <Movie> recommendFromWishlist() const;
-	std::vector <Movie> recommendFromSeen() const;
-	~RecommendedPage();
+
+	inline RecommendedPage(const User& currentUser) :
+		m_currentUser{ currentUser }
+	{
+	}
+
+	inline std::vector<Movie> recommendWishlistMovies() const
+	{
+
+		StorageWishlists tableWishlist = Storages::getInstance().getWishlistStorage();
+		using namespace sqlite_orm;
+		std::unordered_set<std::string> TypeWishlist;
+		auto tableMovies = Storages::getInstance().getMovieStorage();
+		std::vector<Movie> allMovies;
+
+		auto objects = tableWishlist.get_all<Wishlist>();
+
+		for (const auto& wishlistMovie : objects)
+		{
+			if (wishlistMovie.m_userName == m_currentUser.GetUserName())
+			{
+				std::vector <Movie> movieList = tableMovies.get_all<Movie>(where
+				(like((&Movie::m_title), wishlistMovie.m_movieTitle)));
+
+				Movie movie = movieList[0];
+				TypeWishlist.insert(movie.m_type);
+			}
+		}
+		for (const auto& type : TypeWishlist)
+		{
+			allMovies = tableMovies.get_all<Movie>(where(like((&Movie::m_type), type)));
+		}
+		return allMovies;
+
+	}
+
+	inline std::vector<Movie> recommendSeenMovies() const
+	{
+
+		StorageSeen tableSeen = Storages::getInstance().getSeenStorage();
+		using namespace sqlite_orm;
+		std::unordered_set<std::string> TypeSeen;
+		auto tableMovies = Storages::getInstance().getMovieStorage();
+		std::vector<Movie> allMovies;
+
+		auto objects = tableSeen.get_all<Seen>();
+
+		for (const auto& seenMovie : objects)
+		{
+			if (seenMovie.m_userName == m_currentUser.GetUserName())
+			{
+				std::vector <Movie> movieList = tableMovies.get_all<Movie>(where
+				(like((&Movie::m_title), seenMovie.m_movieTitle)));
+
+				Movie movie = movieList[0];
+				TypeSeen.insert(movie.m_type);
+			}
+		}
+		for (const auto& type : TypeSeen)
+		{
+			allMovies = tableMovies.get_all<Movie>(where(like((&Movie::m_type), type)));
+		}
+		return allMovies;
+
+	}
+	~RecommendedPage() = default;
 private:
-	Storages* m_storageLocation;
 	User m_currentUser;
 };
