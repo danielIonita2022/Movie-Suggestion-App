ad7b1f481478ac763cc8b864f46420a8965a3548
RazvanRenghea <razvanrenghea@yahoo.com>
2022-12-11 18:35:26 +0200

    Create ui_Register_GUI.h
    This is the class which contains all the objects for our register window

---
+++ GUI/ui_Register_GUI.h
@@ -0,0 +1,335 @@
+/********************************************************************************
+** Form generated from reading UI file 'Register_GUI.ui'
+**
+** Created by: Qt User Interface Compiler version 6.4.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_REGISTER_GUI_H
+#define UI_REGISTER_GUI_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QScrollArea>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Register_GUI
+{
+public:
+    QScrollArea *scrollArea;
+    QWidget *scrollAreaWidgetContents;
+    QGroupBox *groupBox;
+    QLabel *label_5;
+    QLabel *label_6;
+    QLineEdit *lineEdit_5;
+    QLabel *label_7;
+    QLineEdit *lineEdit_6;
+    QLabel *label_8;
+    QCheckBox *checkBox;
+    QCheckBox *checkBox_2;
+    QCheckBox *checkBox_3;
+    QCheckBox *checkBox_4;
+    QCheckBox *checkBox_5;
+    QCheckBox *checkBox_6;
+    QCheckBox *checkBox_7;
+    QCheckBox *checkBox_8;
+    QCheckBox *checkBox_9;
+    QCheckBox *checkBox_10;
+    QCheckBox *checkBox_11;
+    QLabel *label_9;
+    QLabel *label_10;
+    QLineEdit *lineEdit_7;
+    QLineEdit *lineEdit_8;
+    QLabel *label_11;
+    QLabel *label_12;
+    QCheckBox *checkBox_12;
+    QCheckBox *checkBox_13;
+    QPushButton *pushButton;
+    QWidget *widget;
+    QVBoxLayout *verticalLayout_3;
+    QVBoxLayout *verticalLayout_2;
+    QVBoxLayout *verticalLayout;
+    QHBoxLayout *horizontalLayout;
+    QLabel *label;
+    QLineEdit *lineEdit;
+    QHBoxLayout *horizontalLayout_2;
+    QLabel *label_2;
+    QLineEdit *lineEdit_2;
+    QHBoxLayout *horizontalLayout_4;
+    QLabel *label_4;
+    QLineEdit *lineEdit_4;
+    QHBoxLayout *horizontalLayout_3;
+    QLabel *label_3;
+    QLineEdit *lineEdit_3;
+
+    void setupUi(QMainWindow *Register_GUI)
+    {
+        if (Register_GUI->objectName().isEmpty())
+            Register_GUI->setObjectName("Register_GUI");
+        Register_GUI->setWindowModality(Qt::WindowModal);
+        Register_GUI->resize(940, 736);
+        Register_GUI->setMinimumSize(QSize(444, 321));
+        Register_GUI->setMaximumSize(QSize(1332, 963));
+        scrollArea = new QScrollArea(Register_GUI);
+        scrollArea->setObjectName("scrollArea");
+        scrollArea->setGeometry(QRect(80, 20, 781, 701));
+        scrollArea->setWidgetResizable(true);
+        scrollAreaWidgetContents = new QWidget();
+        scrollAreaWidgetContents->setObjectName("scrollAreaWidgetContents");
+        scrollAreaWidgetContents->setGeometry(QRect(0, 0, 779, 699));
+        groupBox = new QGroupBox(scrollAreaWidgetContents);
+        groupBox->setObjectName("groupBox");
+        groupBox->setGeometry(QRect(0, 0, 761, 701));
+        label_5 = new QLabel(groupBox);
+        label_5->setObjectName("label_5");
+        label_5->setGeometry(QRect(210, 180, 331, 41));
+        QFont font;
+        font.setFamilies({QString::fromUtf8("Cambria")});
+        font.setPointSize(11);
+        label_5->setFont(font);
+        label_6 = new QLabel(groupBox);
+        label_6->setObjectName("label_6");
+        label_6->setGeometry(QRect(210, 240, 211, 16));
+        label_6->setFont(font);
+        lineEdit_5 = new QLineEdit(groupBox);
+        lineEdit_5->setObjectName("lineEdit_5");
+        lineEdit_5->setGeometry(QRect(220, 270, 151, 20));
+        label_7 = new QLabel(groupBox);
+        label_7->setObjectName("label_7");
+        label_7->setGeometry(QRect(210, 300, 251, 21));
+        label_7->setFont(font);
+        lineEdit_6 = new QLineEdit(groupBox);
+        lineEdit_6->setObjectName("lineEdit_6");
+        lineEdit_6->setGeometry(QRect(220, 330, 151, 21));
+        label_8 = new QLabel(groupBox);
+        label_8->setObjectName("label_8");
+        label_8->setGeometry(QRect(210, 360, 271, 16));
+        label_8->setFont(font);
+        checkBox = new QCheckBox(groupBox);
+        checkBox->setObjectName("checkBox");
+        checkBox->setGeometry(QRect(220, 390, 75, 20));
+        checkBox->setFont(font);
+        checkBox_2 = new QCheckBox(groupBox);
+        checkBox_2->setObjectName("checkBox_2");
+        checkBox_2->setGeometry(QRect(300, 390, 75, 20));
+        checkBox_2->setFont(font);
+        checkBox_3 = new QCheckBox(groupBox);
+        checkBox_3->setObjectName("checkBox_3");
+        checkBox_3->setGeometry(QRect(380, 390, 75, 20));
+        checkBox_3->setFont(font);
+        checkBox_4 = new QCheckBox(groupBox);
+        checkBox_4->setObjectName("checkBox_4");
+        checkBox_4->setGeometry(QRect(220, 420, 75, 20));
+        checkBox_4->setFont(font);
+        checkBox_5 = new QCheckBox(groupBox);
+        checkBox_5->setObjectName("checkBox_5");
+        checkBox_5->setGeometry(QRect(450, 390, 75, 20));
+        checkBox_5->setFont(font);
+        checkBox_6 = new QCheckBox(groupBox);
+        checkBox_6->setObjectName("checkBox_6");
+        checkBox_6->setGeometry(QRect(300, 420, 75, 20));
+        checkBox_6->setFont(font);
+        checkBox_7 = new QCheckBox(groupBox);
+        checkBox_7->setObjectName("checkBox_7");
+        checkBox_7->setGeometry(QRect(240, 450, 91, 20));
+        checkBox_7->setFont(font);
+        checkBox_8 = new QCheckBox(groupBox);
+        checkBox_8->setObjectName("checkBox_8");
+        checkBox_8->setGeometry(QRect(380, 420, 75, 20));
+        checkBox_8->setFont(font);
+        checkBox_9 = new QCheckBox(groupBox);
+        checkBox_9->setObjectName("checkBox_9");
+        checkBox_9->setGeometry(QRect(340, 450, 75, 20));
+        checkBox_9->setFont(font);
+        checkBox_10 = new QCheckBox(groupBox);
+        checkBox_10->setObjectName("checkBox_10");
+        checkBox_10->setGeometry(QRect(440, 450, 75, 20));
+        checkBox_10->setFont(font);
+        checkBox_11 = new QCheckBox(groupBox);
+        checkBox_11->setObjectName("checkBox_11");
+        checkBox_11->setGeometry(QRect(450, 420, 101, 20));
+        checkBox_11->setFont(font);
+        label_9 = new QLabel(groupBox);
+        label_9->setObjectName("label_9");
+        label_9->setGeometry(QRect(210, 490, 271, 16));
+        label_9->setFont(font);
+        label_10 = new QLabel(groupBox);
+        label_10->setObjectName("label_10");
+        label_10->setGeometry(QRect(210, 520, 421, 16));
+        label_10->setFont(font);
+        lineEdit_7 = new QLineEdit(groupBox);
+        lineEdit_7->setObjectName("lineEdit_7");
+        lineEdit_7->setGeometry(QRect(220, 550, 61, 21));
+        lineEdit_8 = new QLineEdit(groupBox);
+        lineEdit_8->setObjectName("lineEdit_8");
+        lineEdit_8->setGeometry(QRect(300, 550, 61, 21));
+        label_11 = new QLabel(groupBox);
+        label_11->setObjectName("label_11");
+        label_11->setGeometry(QRect(290, 550, 21, 16));
+        label_11->setFont(font);
+        label_12 = new QLabel(groupBox);
+        label_12->setObjectName("label_12");
+        label_12->setGeometry(QRect(210, 580, 381, 16));
+        label_12->setFont(font);
+        checkBox_12 = new QCheckBox(groupBox);
+        checkBox_12->setObjectName("checkBox_12");
+        checkBox_12->setGeometry(QRect(220, 610, 75, 20));
+        checkBox_12->setFont(font);
+        checkBox_13 = new QCheckBox(groupBox);
+        checkBox_13->setObjectName("checkBox_13");
+        checkBox_13->setGeometry(QRect(300, 610, 75, 20));
+        checkBox_13->setFont(font);
+        pushButton = new QPushButton(groupBox);
+        pushButton->setObjectName("pushButton");
+        pushButton->setGeometry(QRect(310, 650, 151, 24));
+        pushButton->setFont(font);
+        pushButton->setCursor(QCursor(Qt::PointingHandCursor));
+        widget = new QWidget(groupBox);
+        widget->setObjectName("widget");
+        widget->setGeometry(QRect(230, 20, 275, 143));
+        verticalLayout_3 = new QVBoxLayout(widget);
+        verticalLayout_3->setObjectName("verticalLayout_3");
+        verticalLayout_3->setContentsMargins(0, 0, 0, 0);
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setSpacing(15);
+        verticalLayout_2->setObjectName("verticalLayout_2");
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setSpacing(15);
+        verticalLayout->setObjectName("verticalLayout");
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName("horizontalLayout");
+        label = new QLabel(widget);
+        label->setObjectName("label");
+        label->setFont(font);
+
+        horizontalLayout->addWidget(label);
+
+        lineEdit = new QLineEdit(widget);
+        lineEdit->setObjectName("lineEdit");
+        QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+        sizePolicy.setHorizontalStretch(12);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(lineEdit->sizePolicy().hasHeightForWidth());
+        lineEdit->setSizePolicy(sizePolicy);
+
+        horizontalLayout->addWidget(lineEdit);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setObjectName("horizontalLayout_2");
+        label_2 = new QLabel(widget);
+        label_2->setObjectName("label_2");
+        label_2->setFont(font);
+
+        horizontalLayout_2->addWidget(label_2);
+
+        lineEdit_2 = new QLineEdit(widget);
+        lineEdit_2->setObjectName("lineEdit_2");
+
+        horizontalLayout_2->addWidget(lineEdit_2);
+
+
+        verticalLayout->addLayout(horizontalLayout_2);
+
+
+        verticalLayout_2->addLayout(verticalLayout);
+
+        horizontalLayout_4 = new QHBoxLayout();
+        horizontalLayout_4->setObjectName("horizontalLayout_4");
+        label_4 = new QLabel(widget);
+        label_4->setObjectName("label_4");
+        label_4->setFont(font);
+
+        horizontalLayout_4->addWidget(label_4);
+
+        lineEdit_4 = new QLineEdit(widget);
+        lineEdit_4->setObjectName("lineEdit_4");
+
+        horizontalLayout_4->addWidget(lineEdit_4);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_4);
+
+        horizontalLayout_3 = new QHBoxLayout();
+        horizontalLayout_3->setObjectName("horizontalLayout_3");
+        label_3 = new QLabel(widget);
+        label_3->setObjectName("label_3");
+        label_3->setFont(font);
+
+        horizontalLayout_3->addWidget(label_3);
+
+        lineEdit_3 = new QLineEdit(widget);
+        lineEdit_3->setObjectName("lineEdit_3");
+
+        horizontalLayout_3->addWidget(lineEdit_3);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_3);
+
+
+        verticalLayout_3->addLayout(verticalLayout_2);
+
+        scrollArea->setWidget(scrollAreaWidgetContents);
+
+        retranslateUi(Register_GUI);
+
+        QMetaObject::connectSlotsByName(Register_GUI);
+    } // setupUi
+
+    void retranslateUi(QMainWindow*Register_GUI)
+    {
+        Register_GUI->setWindowTitle(QCoreApplication::translate("Register_GUI", "Register", nullptr));
+        groupBox->setTitle(QString());
+        label_5->setText(QCoreApplication::translate("Register_GUI", "To help us finding the best recomandations for you\n"
+"              please answer the following quiestions:", nullptr));
+        label_6->setText(QCoreApplication::translate("Register_GUI", "1. What is your favourite movie?", nullptr));
+        label_7->setText(QCoreApplication::translate("Register_GUI", "2. Who is your favourite actor/actress?", nullptr));
+        label_8->setText(QCoreApplication::translate("Register_GUI", "3. Which one of these do you prefer?", nullptr));
+        checkBox->setText(QCoreApplication::translate("Register_GUI", "drama", nullptr));
+        checkBox_2->setText(QCoreApplication::translate("Register_GUI", "action", nullptr));
+        checkBox_3->setText(QCoreApplication::translate("Register_GUI", "SF", nullptr));
+        checkBox_4->setText(QCoreApplication::translate("Register_GUI", "comedy", nullptr));
+        checkBox_5->setText(QCoreApplication::translate("Register_GUI", "thriller", nullptr));
+        checkBox_6->setText(QCoreApplication::translate("Register_GUI", "fantasy", nullptr));
+        checkBox_7->setText(QCoreApplication::translate("Register_GUI", "animation", nullptr));
+        checkBox_8->setText(QCoreApplication::translate("Register_GUI", "horror", nullptr));
+        checkBox_9->setText(QCoreApplication::translate("Register_GUI", "romance", nullptr));
+        checkBox_10->setText(QCoreApplication::translate("Register_GUI", "mistery", nullptr));
+        checkBox_11->setText(QCoreApplication::translate("Register_GUI", "adventure", nullptr));
+        label_9->setText(QCoreApplication::translate("Register_GUI", "4. Does the release year matter to you?", nullptr));
+        label_10->setText(QCoreApplication::translate("Register_GUI", "If it does, please introduce years between 1960 and 2022.", nullptr));
+        lineEdit_7->setText(QString());
+        lineEdit_8->setText(QString());
+        label_11->setText(QCoreApplication::translate("Register_GUI", "-", nullptr));
+        label_12->setText(QCoreApplication::translate("Register_GUI", "5. Which one do you prefer more? Movies or TV-Shows?", nullptr));
+        checkBox_12->setText(QCoreApplication::translate("Register_GUI", "movies", nullptr));
+        checkBox_13->setText(QCoreApplication::translate("Register_GUI", "tv-shows", nullptr));
+        pushButton->setText(QCoreApplication::translate("Register_GUI", "Complete registration", nullptr));
+        label->setText(QCoreApplication::translate("Register_GUI", "First Name", nullptr));
+        lineEdit->setText(QString());
+        label_2->setText(QCoreApplication::translate("Register_GUI", "Last Name ", nullptr));
+        label_4->setText(QCoreApplication::translate("Register_GUI", "Username  ", nullptr));
+        label_3->setText(QCoreApplication::translate("Register_GUI", "Password   ", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Register_GUI: public Ui_Register_GUI {};
+} // namespace Ui
+QT_END_NAMESPACE
+
+#endif // UI_REGISTER_GUI_H
