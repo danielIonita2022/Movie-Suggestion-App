eb943b1448107a4e1b020a0c54b2bb6a33901e71
danielIonita2022 <daniel.ionita@student.unitbv.ro>
2023-01-07 19:05:43 +0200

    Update Storages.h
    Remade the Storages class, which has methods for updating each table and populating the movies table.

--- Storages/Storages.h
+++ Storages/Storages.h
@@ -1,28 +1,18 @@
 #pragma once
-#include "Movie.h"
-#include "User.h"
-#include "Wishlist.h"
-#include "Seen.h"
+#pragma warning(disable : 4996)
+#include "CreateStorage.h"
 
-using StorageMovies = decltype(Movie::CreateTable());
-using StorageUsers = decltype(User::CreateTable());
-using StorageWishlists = decltype(Wishlist::CreateTable());
-using StorageSeen = decltype(Seen::CreateTable());
+using namespace sqlite_orm;
 
 class Storages
 {
 public:
-	StorageMovies getMovieStorage() const;
-	StorageUsers getUserStorage() const;
-	StorageWishlists getWishlistStorage() const;
-	StorageSeen getSeenStorage() const;
-	static Storages& getInstance();
-	Storages(const Storages& storageInstance) = delete;
-
-private:
-	Storages();
-	const StorageMovies m_storageMovies = Movie::CreateTable();
-	const StorageUsers m_storageUsers = User::CreateTable();
-	const StorageWishlists m_storageWishlists = Wishlist::CreateTable();
-	const StorageSeen m_storageSeen = Seen::CreateTable();
+    
+    using DB = decltype(CreateStorage());
+    Storages() = default;
+    static DB getStorage();
+    static void PopulateMovies();
+    static void UpdateUsers(const User& user);
+    static void UpdateSeen(Seen& seen);
+    static void UpdateWishlist(Wishlist& wishlist);
 };
