da003fd8f575809fb316c3166beb89e65671eeb5
RazvanRenghea <razvanrenghea@yahoo.com>
2023-01-14 13:48:53 +0200

    Update objects for 2 windows
    Here I updated some characteristics of the labels from Recommendations_Page_GUI window and Get_sim_mov_ser_page_GUI window in order to display the movie titles correctly and added a back_to_homepage button for Recommendations_Page_GUI window

--- GUI/ui_Recommendations_Page_GUI.h
+++ GUI/ui_Recommendations_Page_GUI.h
@@ -15,11 +15,8 @@
 #include <QtWidgets/QGridLayout>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QMenu>
-#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QScrollArea>
-#include <QtWidgets/QStatusBar>
-#include <QtWidgets/QToolBar>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -46,16 +43,13 @@ public:
     QLabel *Movie9;
     QLabel *Movie10;
     QLabel *Title_text;
-    QMenuBar *menuBar;
-    QMenu *menuHome;
-    QToolBar *mainToolBar;
-    QStatusBar *statusBar;
+    QPushButton *Back_to_homepage;
 
     void setupUi(QMainWindow *Recommendations_Page_GUIClass)
     {
         if (Recommendations_Page_GUIClass->objectName().isEmpty())
             Recommendations_Page_GUIClass->setObjectName("Recommendations_Page_GUIClass");
-        Recommendations_Page_GUIClass->resize(673, 488);
+        Recommendations_Page_GUIClass->resize(832, 481);
         actionHome = new QAction(Recommendations_Page_GUIClass);
         actionHome->setObjectName("actionHome");
         actionMy_profile = new QAction(Recommendations_Page_GUIClass);
@@ -75,80 +69,69 @@ public:
         scrollArea->setWidgetResizable(true);
         scrollAreaWidgetContents = new QWidget();
         scrollAreaWidgetContents->setObjectName("scrollAreaWidgetContents");
-        scrollAreaWidgetContents->setGeometry(QRect(0, 0, 653, 405));
+        scrollAreaWidgetContents->setGeometry(QRect(0, 0, 812, 461));
         Movie1 = new QLabel(scrollAreaWidgetContents);
         Movie1->setObjectName("Movie1");
-        Movie1->setGeometry(QRect(40, 40, 181, 51));
+        Movie1->setGeometry(QRect(20, 40, 311, 51));
         QFont font;
         font.setFamilies({QString::fromUtf8("Cambria")});
-        font.setPointSize(11);
+        font.setPointSize(10);
         Movie1->setFont(font);
         Movie2 = new QLabel(scrollAreaWidgetContents);
         Movie2->setObjectName("Movie2");
-        Movie2->setGeometry(QRect(40, 130, 181, 51));
+        Movie2->setGeometry(QRect(20, 130, 311, 51));
         Movie2->setFont(font);
         Movie3 = new QLabel(scrollAreaWidgetContents);
         Movie3->setObjectName("Movie3");
-        Movie3->setGeometry(QRect(40, 220, 181, 51));
+        Movie3->setGeometry(QRect(20, 220, 311, 51));
         Movie3->setFont(font);
         Movie4 = new QLabel(scrollAreaWidgetContents);
         Movie4->setObjectName("Movie4");
-        Movie4->setGeometry(QRect(40, 310, 181, 51));
+        Movie4->setGeometry(QRect(20, 310, 311, 51));
         Movie4->setFont(font);
         Movie5 = new QLabel(scrollAreaWidgetContents);
         Movie5->setObjectName("Movie5");
-        Movie5->setGeometry(QRect(40, 400, 181, 51));
+        Movie5->setGeometry(QRect(20, 400, 311, 51));
         Movie5->setFont(font);
         Movie6 = new QLabel(scrollAreaWidgetContents);
         Movie6->setObjectName("Movie6");
-        Movie6->setGeometry(QRect(450, 40, 181, 51));
+        Movie6->setGeometry(QRect(490, 40, 311, 51));
         Movie6->setFont(font);
         Movie7 = new QLabel(scrollAreaWidgetContents);
         Movie7->setObjectName("Movie7");
-        Movie7->setGeometry(QRect(450, 130, 181, 51));
+        Movie7->setGeometry(QRect(490, 130, 311, 51));
         Movie7->setFont(font);
         Movie8 = new QLabel(scrollAreaWidgetContents);
         Movie8->setObjectName("Movie8");
-        Movie8->setGeometry(QRect(450, 220, 181, 51));
+        Movie8->setGeometry(QRect(490, 220, 311, 51));
         Movie8->setFont(font);
         Movie9 = new QLabel(scrollAreaWidgetContents);
         Movie9->setObjectName("Movie9");
-        Movie9->setGeometry(QRect(450, 310, 181, 51));
+        Movie9->setGeometry(QRect(490, 310, 311, 51));
         Movie9->setFont(font);
         Movie10 = new QLabel(scrollAreaWidgetContents);
         Movie10->setObjectName("Movie10");
-        Movie10->setGeometry(QRect(450, 400, 181, 51));
+        Movie10->setGeometry(QRect(490, 400, 311, 51));
         Movie10->setFont(font);
         Title_text = new QLabel(scrollAreaWidgetContents);
         Title_text->setObjectName("Title_text");
-        Title_text->setGeometry(QRect(40, 0, 581, 31));
+        Title_text->setGeometry(QRect(160, 0, 581, 31));
         QFont font1;
         font1.setFamilies({QString::fromUtf8("Terminal")});
         font1.setPointSize(12);
         Title_text->setFont(font1);
+        Back_to_homepage = new QPushButton(scrollAreaWidgetContents);
+        Back_to_homepage->setObjectName("Back_to_homepage");
+        Back_to_homepage->setGeometry(QRect(0, 0, 131, 31));
+        QFont font2;
+        font2.setFamilies({QString::fromUtf8("Cambria")});
+        font2.setPointSize(11);
+        Back_to_homepage->setFont(font2);
         scrollArea->setWidget(scrollAreaWidgetContents);
 
         gridLayout->addWidget(scrollArea, 0, 1, 1, 1);
 
         Recommendations_Page_GUIClass->setCentralWidget(centralWidget);
-        menuBar = new QMenuBar(Recommendations_Page_GUIClass);
-        menuBar->setObjectName("menuBar");
-        menuBar->setGeometry(QRect(0, 0, 673, 26));
-        menuHome = new QMenu(menuBar);
-        menuHome->setObjectName("menuHome");
-        Recommendations_Page_GUIClass->setMenuBar(menuBar);
-        mainToolBar = new QToolBar(Recommendations_Page_GUIClass);
-        mainToolBar->setObjectName("mainToolBar");
-        Recommendations_Page_GUIClass->addToolBar(Qt::TopToolBarArea, mainToolBar);
-        statusBar = new QStatusBar(Recommendations_Page_GUIClass);
-        statusBar->setObjectName("statusBar");
-        Recommendations_Page_GUIClass->setStatusBar(statusBar);
-
-        menuBar->addAction(menuHome->menuAction());
-        menuHome->addAction(actionHome);
-        menuHome->addAction(actionMy_profile);
-        menuHome->addAction(actionView_recommendations);
-        menuHome->addAction(actionSign_out);
 
         retranslateUi(Recommendations_Page_GUIClass);
 
@@ -173,7 +156,7 @@ public:
         Movie9->setText(QString());
         Movie10->setText(QString());
         Title_text->setText(QCoreApplication::translate("Recommendations_Page_GUIClass", "Here are 10 movies/series reccomanded for you :)", nullptr));
-        menuHome->setTitle(QCoreApplication::translate("Recommendations_Page_GUIClass", "|||", nullptr));
+        Back_to_homepage->setText(QCoreApplication::translate("Recommendations_Page_GUIClass", "Back to homepage", nullptr));
     } // retranslateUi
 
 };
