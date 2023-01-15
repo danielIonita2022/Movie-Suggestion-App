39113e47165f4f98482d635837359dfec5ae716f
ana-mariagg <ana.grigoras@student.unitbv.ro>
2023-01-15 15:40:55 +0200

    Update main.cpp
    Implemented the functions to test profiling.

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -18,3 +18,130 @@ int main()
     return 0;
 }
 
+//mergem pe cazul in care parola are nr corect de caractere si verificam doar uppercase, lowercase si isdigit
+
+//uppercase basic
+void analyzePasswUpperCaseCustom(const std::vector<User>& users)
+{
+    PROFILE_FUNCTION();
+    uint16_t corecte = 0;
+    uint16_t gresite = 0;
+    for (size_t i = 0; i < users.size(); ++i)
+    {
+        if (PasswordValidator::CustomPasswordValidation::verifyUpperCase(users[i].GetPassword()))
+        {
+            corecte++;
+        }
+        else
+        {
+            gresite++;
+        }
+    }
+    std::cout << "The number of correct passwords regarding upper case letter is " << corecte << "\n";
+    std::cout << "The number of incorrect passwords regarding upper case letter is " << gresite << "\n";
+}
+
+//uppercase regex
+void analyzePasswUpperCaseRegex(const std::vector<User>& users)
+{
+    PROFILE_FUNCTION();
+    uint16_t corecte = 0;
+    uint16_t gresite = 0;
+    for (size_t i = 0; i < users.size(); ++i)
+    {
+        if (PasswordValidator::RegexPasswordValidation::verifyUpperCase(users[i].GetPassword()))
+        {
+            corecte++;
+        }
+        else
+        {
+            gresite++;
+        }
+    }
+    std::cout << "The number of correct passwords regarding upper case letter is " << corecte << "\n";
+    std::cout << "The number of incorrect passwords regarding upper case letter is " << gresite << "\n";
+}
+
+//lowercase basic
+void analyzePasswLowerCaseCustom(const std::vector<User>& users)
+{
+    PROFILE_FUNCTION();
+    uint16_t corecte = 0;
+    uint16_t gresite = 0;
+    for (size_t i = 0; i < users.size(); ++i)
+    {
+        if (PasswordValidator::CustomPasswordValidation::verifyLowerCase(users[i].GetPassword()))
+        {
+            corecte++;
+        }
+        else
+        {
+            gresite++;
+        }
+    }
+    std::cout << "The number of correct passwords regarding lower case letter is " << corecte << "\n";
+    std::cout << "The number of incorrect passwords regarding lower case letter is " << gresite << "\n";
+}
+
+//lowercase regex
+void analyzePasswLowerCaseRegex(const std::vector<User>& users)
+{
+    PROFILE_FUNCTION();
+    uint16_t corecte = 0;
+    uint16_t gresite = 0;
+    for (size_t i = 0; i < users.size(); ++i)
+    {
+        if (PasswordValidator::RegexPasswordValidation::verifyLowerCase(users[i].GetPassword()))
+        {
+            corecte++;
+        }
+        else
+        {
+            gresite++;
+        }
+    }
+    std::cout << "The number of correct passwords regarding lower case letter is " << corecte << "\n";
+    std::cout << "The number of incorrect passwords regarding lower case letter is " << gresite << "\n";
+}
+
+//isdigit basic
+void analyzePasswDigitCustom(const std::vector<User>& users)
+{
+    PROFILE_FUNCTION();
+    uint16_t corecte = 0;
+    uint16_t gresite = 0;
+    for (size_t i = 0; i < users.size(); ++i)
+    {
+        if (PasswordValidator::CustomPasswordValidation::verifyIsDigit(users[i].GetPassword()))
+        {
+            corecte++;
+        }
+        else
+        {
+            gresite++;
+        }
+    }
+    std::cout << "The number of correct passwords regarding the digit is " << corecte << "\n";
+    std::cout << "The number of incorrect passwords regarding the digit is " << gresite << "\n";
+}
+
+//isdigit regex
+void analyzePasswDigitRegex(const std::vector<User>& users)
+{
+    PROFILE_FUNCTION();
+    uint16_t corecte = 0;
+    uint16_t gresite = 0;
+    for (size_t i = 0; i < users.size(); ++i)
+    {
+        if (PasswordValidator::RegexPasswordValidation::verifyIsDigit(users[i].GetPassword()))
+        {
+            corecte++;
+        }
+        else
+        {
+            gresite++;
+        }
+    }
+    std::cout << "The number of correct passwords regarding the digit is " << corecte << "\n";
+    std::cout << "The number of incorrect passwords regarding the digit is " << gresite << "\n";
+}
